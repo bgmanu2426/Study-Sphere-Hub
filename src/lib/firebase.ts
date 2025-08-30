79b8ecc2d94667bb256c9f4e04a7365b12c1655c
Sneha Hm <snehahanjgimath@gmail.com>
2025-08-30 19:38:43 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage, ref, listAll, getDownloadURL, getMetadata, updateMetadata, 
 import { Subject, ResourceFile } from "./data";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWfHPsEOU50o5oLLNKF0YycHlyNF6gjKA",
-  authDomain: "vtu-assistant-rkmf1.firebaseapp.com",
-  projectId: "vtu-assistant-rkmf1",
-  storageBucket: "vtu-assistant-rkmf1.appspot.com",
-  messagingSenderId: "998939257663",
-  appId: "1:998939257663:web:1b33f669b722eaa991c18a",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 
