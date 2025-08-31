789384c44b8e1a63eea4d30384f1862b37c9a040
Sneha Hm <snehahanjgimath@gmail.com>
2025-08-31 15:39:06 +0000

    upload-form.tsx:215   POST https://api.cloudinary.com/v1_1/djzpd5k3q/upl

--- src/lib/cloudinary-server.ts
+++ src/lib/cloudinary-server.ts
@@ -3,8 +3,8 @@ import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.SERVER_CLOUDINARY_API_KEY,
+  api_secret: process.env.SERVER_CLOUDINARY_API_SECRET,
   secure: true,
 });
 
